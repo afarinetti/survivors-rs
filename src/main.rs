c35a16660bdb4c66f5c24f290a290a36ab16e90d
Antonio Farinetti <afarinetti@users.noreply.github.com>
2023-09-30 22:36:11 -0500

    Adding prototype implementation using bevy_ecs_ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,150 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Rust Survivors-Like".into(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+        )
+        .add_plugins((
+            LdtkPlugin,
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (
+            character_movement,
+            bevy::window::close_on_esc,
+            camera_fit_inside_current_level,
+            // spawn_wall_collision
+        ))
+        .insert_resource(LevelSelection::default())
+        .register_ldtk_int_cell_for_layer::<WallBundle>("walls", 1)
+        .register_ldtk_entity_for_layer::<PlayerBundle>("entities", "player")
+        .run();
 }
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    // spawn the camera
+    let camera = Camera2dBundle::default();
+    commands.spawn(camera);
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("levels.ldtk"),
+        ..default()
+    });
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Wall;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Player;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Enemy;
+
+#[derive(Clone, Default, Bundle, LdtkEntity)]
+pub struct PlayerBundle {
+    player: Player,
+    #[sprite_sheet_bundle]
+    sprite_bundle: SpriteSheetBundle,
+}
+
+#[derive(Clone, Default, Bundle, LdtkIntCell)]
+pub struct WallBundle {
+    wall: Wall,
+}
+
+// pub fn spawn_wall_collision(
+//     mut commands: Commands,
+//     wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
+//     parent_query: Query<&Parent, Without<Wall>>,
+//     level_query: Query<(Entity, &Handle<LdtkLevel>)>,
+//     levels: Res<Assets<LdtkLevel>>,
+// ) {
+//     wall_query.for_each(|(&grid_coords, parent)| {
+//         println!("wall: {}, {}", grid_coords.x, grid_coords.y);
+//     });
+// }
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Player)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
+            transform.translation.y += 128.0 * time.delta_seconds();
+            // transform.translation += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
+            transform.translation.y -= 128.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
+            transform.translation.x += 128.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) || input.pressed(KeyCode::Left) {
+            transform.translation.x -= 128.0 * time.delta_seconds();
+        }
+    }
+}
+
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+
+pub fn camera_fit_inside_current_level(
+    mut camera_query: Query<(&mut OrthographicProjection, &mut Transform), Without<Player>>,
+    player_query: Query<&Transform, With<Player>>,
+    level_query: Query<(&Transform, &Handle<LdtkLevel>), (Without<OrthographicProjection>, Without<Player>)>,
+    level_selection: Res<LevelSelection>,
+    ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+    if let Ok(Transform { translation: player_translation, .. }) = player_query.get_single() {
+        let player_translation = *player_translation;
+
+        let (mut orthographic_projection, mut camera_transform) = camera_query.single_mut();
+
+        for (level_transform, level_handle) in &level_query {
+            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+                let level = &ldtk_level.level;
+                if level_selection.is_match(&0, level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+                    orthographic_projection.viewport_origin = Vec2::ZERO;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        let height = (level.px_hei as f32 / 9.).round() * 9.;
+                        let width = height * ASPECT_RATIO;
+                        orthographic_projection.scaling_mode =
+                            bevy::render::camera::ScalingMode::Fixed { width, height };
+                        camera_transform.translation.x =
+                            (player_translation.x - level_transform.translation.x - width / 2.)
+                                .clamp(0., level.px_wid as f32 - width);
+                        camera_transform.translation.y = 0.;
+                    } else {
+                        // level is taller than the screen
+                        let width = (level.px_wid as f32 / 16.).round() * 16.;
+                        let height = width / ASPECT_RATIO;
+                        orthographic_projection.scaling_mode =
+                            bevy::render::camera::ScalingMode::Fixed { width, height };
+                        camera_transform.translation.y =
+                            (player_translation.y - level_transform.translation.y - height / 2.)
+                                .clamp(0., level.px_hei as f32 - height);
+                        camera_transform.translation.x = 0.;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
+            }
+        }
+    }
+}
+
